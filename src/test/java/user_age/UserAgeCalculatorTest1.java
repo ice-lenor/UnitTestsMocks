71a10cae1e39eca84c52c8d0eb4cafeb905d6d47
Elena Nikolaeva <elena.nikolaeva@booking.com>
2019-09-12 13:53:05 +0200

    added more comments in tests

--- src/test/java/user_age/UserAgeCalculatorTest1.java
+++ src/test/java/user_age/UserAgeCalculatorTest1.java
@@ -1,11 +1,11 @@
 package user_age;
 
 import org.junit.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+// This is our first try in writing unit tests for our UserAgeCalculator.calculateUserAge method.
+// These tests are actually integration tests, because they access the "real" UsersDatabase.
 public class UserAgeCalculatorTest1 {
 
     @Test
@@ -13,10 +13,27 @@ public class UserAgeCalculatorTest1 {
 
         UsersDatabase usersDatabase = new UsersDatabase();
         UserAgeCalculator calculator = new UserAgeCalculator(usersDatabase);
+        Date dateTo = DateHelpers.parseDate("2019-01-01");
 
-        Date now = DateHelpers.parseDate("2019-09-04");
-        int resultAge = calculator.calculateUserAge(1, now);
+        // User Emma has id = 1, and we're calculating her age.
+        // What happens if Emma closes her account?
+        int resultAge = calculator.calculateUserAge(1, dateTo);
 
-        assertEquals(13058, resultAge);
+        assertEquals(12813, resultAge);
+    }
+
+    @Test
+    public void nonExistingUser() {
+
+        UsersDatabase usersDatabase = new UsersDatabase();
+        UserAgeCalculator calculator = new UserAgeCalculator(usersDatabase);
+
+        Date dateTo = DateHelpers.parseDate("2019-09-12");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            // User with id=42 doesn't exist in our database just yet,
+            // but what happens if our app is popular and new users keep coming?
+            int resultAge = calculator.calculateUserAge(42, dateTo);
+        });
     }
 }
--- src/test/java/user_age/UserAgeCalculatorTest2.java
+++ src/test/java/user_age/UserAgeCalculatorTest2.java
@@ -5,19 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.mockito.Mockito;
 import java.util.Date;
 
+// This is our second try in writing unit tests for our UserAgeCalculator.calculateUserAge method.
+// We are using mocks here to replace the UsersDatabase with a "fake" one, a new one for each test.
+// This lets us explore more scenarios that were not possible when using "real" data,
+// and our tests are more stable because we control the environment now.
 public class UserAgeCalculatorTest2 {
 
     @Test
     public void userBabyAge() {
 
+        // We are creating a pretend database, only for our test.
         UsersDatabase usersDatabase = Mockito.mock(UsersDatabase.class);
+        // We are creating a fake user, purely for our test.
         int userId = 42;
         User user = new User(userId, "Baby", DateHelpers.parseDate("2019-09-01"));
+        // Our fake database is going to return this imaginary user.
         Mockito.when(usersDatabase.getUserById(userId)).thenReturn(user);
+
         Date dateTo = DateHelpers.parseDate("2019-09-12");
 
+        // We calculate the result.
         UserAgeCalculator calculator = new UserAgeCalculator(usersDatabase);
-
         int resultAge = calculator.calculateUserAge(userId, dateTo);
 
         assertEquals(11, resultAge);
@@ -33,7 +41,6 @@ public class UserAgeCalculatorTest2 {
         Date dateTo = DateHelpers.parseDate("2019-09-12");
 
         UserAgeCalculator calculator = new UserAgeCalculator(usersDatabase);
-
         int resultAge = calculator.calculateUserAge(userId, dateTo);
 
         assertEquals(365, resultAge);
@@ -43,12 +50,16 @@ public class UserAgeCalculatorTest2 {
     public void userIsNull() {
 
         UsersDatabase usersDatabase = Mockito.mock(UsersDatabase.class);
+        // User with this id is null = not exist.
+        // We know this for sure because we have created a mock database.
         int userId = 42;
         Mockito.when(usersDatabase.getUserById(userId)).thenReturn(null);
+
         Date dateTo = DateHelpers.parseDate("2019-09-12");
 
         UserAgeCalculator calculator = new UserAgeCalculator(usersDatabase);
 
+        // We expect the exception telling us that the user does not exist.
         assertThrows(IllegalArgumentException.class, () -> {
             int resultAge = calculator.calculateUserAge(userId, dateTo);
         });
@@ -58,15 +69,18 @@ public class UserAgeCalculatorTest2 {
     public void userIsFromTheFuture() {
 
         UsersDatabase usersDatabase = Mockito.mock(UsersDatabase.class);
+        // We might not have a baby-yet-to-be-born in our real database,
+        // but we can create a test one.
         int userId = 42;
         User user = new User(userId, "Someone", DateHelpers.parseDate("2019-09-13"));
         Mockito.when(usersDatabase.getUserById(userId)).thenReturn(user);
+
         Date dateTo = DateHelpers.parseDate("2019-09-12");
 
         UserAgeCalculator calculator = new UserAgeCalculator(usersDatabase);
         int resultAge = calculator.calculateUserAge(userId, dateTo);
 
-        assertEquals(-1, resultAge);
+        assertEquals(-1, resultAge); // the user is not born yet!
     }
 
 }
